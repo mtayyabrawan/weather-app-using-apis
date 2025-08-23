6655b9bdcd06c90652a043cb84e94036f79622e1
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-23 22:41:49 +0500

    refactor: rename project to weather-app and update dependencies
    - Changed project name from mini-project to weather-app in package.json.
    - Updated React and React DOM to version 19.1.1.
    - Updated various devDependencies to their latest versions.
    - Modified App component to include Vite and React logos with a counter.
    - Added index.css for global styles.
    - Wrapped App component in StrictMode for better development practices.
    - Adjusted Vite configuration for consistency.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/weather-app-using-apis/',  // replace with your repository name
-  plugins: [react()],
-})
+  base: "/weather-app-using-apis/",
+  plugins: [react()]
+});
