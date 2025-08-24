bd2158d2688aa1d41cd360925702fb0e05687fac
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-24 13:24:37 +0500

    fix: corrected API endpoint in getWeatherByLocation function to use the correct version

--- src/api/getWeatherByLocation.api.js
+++ src/api/getWeatherByLocation.api.js
@@ -3,7 +3,7 @@ const weather_api_key = import.meta.env.VITE_WEATHER_API_KEY;
 async function getWeatherByLocation(lat, lon) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${weather_api_key}`,
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weather_api_key}`,
     );
     const data = await response.json();
     return { resStatus: true, weather: data };
