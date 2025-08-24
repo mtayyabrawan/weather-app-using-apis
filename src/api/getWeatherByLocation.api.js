025b6d16b3284152bc713b017f9ce52204beef2c
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-24 11:26:08 +0500

    feat: added sonner dependency and implement getWeatherByLocation API function

---
+++ src/api/getWeatherByLocation.api.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+const weather_api_key = import.meta.env.VITE_WEATHER_API_KEY;
+async function getWeatherByLocation(lat, lon) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${weather_api_key}`,
+    );
+    const data = await response.json();
+    return { resStatus: true, weather: data };
+  } catch (_) {
+    return { resStatus: false, error: "Error fetching weather data" };
+  }
+}
+
+export default getWeatherByLocation;
