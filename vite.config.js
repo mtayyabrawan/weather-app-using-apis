4437252967942be8b27322bc53527831904893a7
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-24 09:01:52 +0500

    feat: integrate Tailwind CSS and remove unused components
    - Added Tailwind CSS and its Vite plugin to the project.
    - Removed unused InfoBox and SearchBox components along with their styles.
    - Deleted unnecessary SVG assets.
    - Updated index.css to use Tailwind CSS for styling.
    - Removed initialData.js as it was not utilized.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/weather-app-using-apis/",
-  plugins: [react()]
+  plugins: [react(), tailwindcss()]
 });
