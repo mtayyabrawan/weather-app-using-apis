869f77cb1d3602a70eb886a660fa77806a75ef6b
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-24 11:08:40 +0500

    feat: implemented getLocationByIP function to fetch location data based on IP address

---
+++ src/api/getLocationByIP.api.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+async function getLocationByIP(ip) {
+  try {
+    const response = await fetch(`https://ipapi.co/${ip}/json/`);
+    const data = await response.json();
+    return { resStatus: true, location: data };
+  } catch (_) {
+    return { resStatus: false, error: "Error fetching location" };
+  }
+}
+
+export default getLocationByIP;
