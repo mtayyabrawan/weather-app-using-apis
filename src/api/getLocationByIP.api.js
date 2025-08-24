5e39d22d3ed05871fb7eb7416cb93d28f178c6a1
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-24 12:47:50 +0500

    feat: updated getLocationByIP function to return structured location data feat: add Toaster component for notifications in WeatherLayout

--- src/api/getLocationByIP.api.js
+++ src/api/getLocationByIP.api.js
@@ -3,7 +3,10 @@ async function getLocationByIP(ip) {
   try {
     const response = await fetch(`https://ipapi.co/${ip}/json/`);
     const data = await response.json();
-    return { resStatus: true, location: data };
+    return {
+      resStatus: true,
+      location: { longitude: data.longitude, latitude: data.latitude },
+    };
   } catch (_) {
     return { resStatus: false, error: "Error fetching location" };
   }
--- src/layouts/WeatherLayout.jsx
+++ src/layouts/WeatherLayout.jsx
@@ -1,8 +1,10 @@
+import { Toaster } from "sonner";
 import Footer from "../components/common/Footer";
 
 function WeatherLayout({ children }) {
   return (
     <div className="w-full bg-cyan-100">
+      <Toaster position="top-right" richColors={true} />
       <main className="min-h-[calc(100vh-2.25rem)]">{children}</main>
       <Footer />
     </div>
